36c351841da42e337805a291206ec678b35eac5a
bibotai <bibotaixsf@gmail.com>
2016-12-18 14:49:59 +0800

    重构，分离HeroesComponent，将AppComponent作为壳

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,14 +8,19 @@ import {AppComponent} from './app.component';
 
 //导入HeroDetailComponent组件
 import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+
+import {HeroService} from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule, FormsModule
   ], //把FormsModule添加到NgModule装饰器的imports数组中
   declarations: [
-    AppComponent, HeroDetailComponent
+    AppComponent, HeroDetailComponent, HeroesComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // 添加HeroService到AppModule的providers数组中，因为我们的每一个视图都需要它。
+  providers: [HeroService]
 })
 export class AppModule {}
